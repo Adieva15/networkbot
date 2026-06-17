1e048e18eadf9c6f2c7281ca192ea5d146dad69d
Dine <adievadine@gmail.com>
2026-06-17 15:16:05 +0300

    настройка handlers.__init__

---
+++ handlers/__init__.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from .sentiment import router as sentiment_router
+
+from .start import router as start_router
+
+
+def register_all_handlers(dp:Router):
+    dp.include_router(start_router)
+    dp.include_router(sentiment_router)
