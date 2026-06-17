e305e39f410385b5c1f10604849263544dde12e2
Dine <adievadine@gmail.com>
2026-06-17 15:13:07 +0300

    настройка sentiment

---
+++ handlers/sentiment.py
@@ -0,0 +1,24 @@
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from functions import sentiment_analysis
+from database import add_record
+from states import MemeState
+
+
+# для связывания URL-адресов с кодом
+router = Router()
+
+
+@router.message(Command("sentiment"))
+async def cmd_sentiment(message:Message, state: FSMContext):
+    await state.set_state(MemeState.waiting_text)
+    await message.answer("Напишите текст для анализа")
+
+@router.message(MemeState.waiting_text, F.text)
+async def analyze(msg:Message, state: FSMContext):
+
+    result = await sentiment_analysis(msg.text)
+    await msg.answer(f"Тональность {result}")
+    add_record(msg.from_user.id, "sentiment", msg.text[:100], result)
