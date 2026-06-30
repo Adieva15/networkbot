57634694c578ad2e222b19b74816e985ed584d9b
Dine <adievadine@gmail.com>
2026-06-30 19:38:22 +0300

    add текствоые опции и настройки сайта

--- functions/__init__.py
+++ functions/__init__.py
@@ -3,4 +3,4 @@ from functions.generate_text import generate_text
 from functions.summarize import summarize_text
 from functions.chat import chat_with_agent
 from functions.text_stats import text_stats
-from functions.text_utils import shuffle_words, to_leetspeak, comзare_texts, transliterate_ru_to_en
+from functions.text_utils import shuffle_words, to_leetspeak, compare_texts, transliterate_ru_to_en
